58fc27b59987078d76ca21e0900d00c9c63ed0d9
Tanapong Latteerasuwan <tanapong.l@kbtg.tech>
2020-09-19 15:03:30 +0700

    save

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +12,9 @@ import (
 
 // git init, git add server.go go.mod, git commit -m "[Nong] init project"
 func main() {
+
 	fmt.Println("hello hometic : I'm Gopher!!")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
@@ -28,6 +31,21 @@ func main() {
 
 }
 
+type Pair struct {
+	DeviceID int64
+	UserID   int64
+}
+
 func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	var p Pair
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+	defer r.Body.Close()
+	fmt.Printf("pair: %#v\n", p)
+
 	w.Write([]byte(`{"status":"active"}`))
 }
