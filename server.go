85e8a0f64ff52da32d268848108b0b75b13b1ce3
Tanapong Latteerasuwan <tanapong.l@kbtg.tech>
2020-09-19 14:23:06 +0700

    save

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -13,12 +14,18 @@ func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 	r := mux.NewRouter()
 	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+	fmt.Println("addr:", addr)
+
 	server := http.Server{
-		Addr:    "127.0.0.1:2009",
+		Addr:    addr,
 		Handler: r,
 	}
+
 	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
+
 }
 
 func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
